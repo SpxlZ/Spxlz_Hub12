783efc12a7493f76ead879609f663f2b17e98d37
SpxlZ <146632787+SpxlZ@users.noreply.github.com>
2023-11-11 20:44:35 +0100

    Update LoaderX.lua

--- LoaderX.lua
+++ LoaderX.lua
@@ -1,6 +1,6 @@
 local list = {
     ["7346416636"] = "https://raw.githubusercontent.com/SpxlZ/Spxlz_Hub12/main/Project1.lua",
-    ["7903991471"] = "https://raw.githubusercontent.com/SpxlZ/Spxlz_Hub12/main/Project3.lua",
+    ["7903991471"] = "https://raw.githubusercontent.com/SpxlZ/Spxlz_Hub12/main/Project3.lua"
 }
 
 for id, url in next, list do
