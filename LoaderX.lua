2c68068344bd7f7ffa2843e8b4c85a4c74a960d5
SpxlZ <146632787+SpxlZ@users.noreply.github.com>
2023-11-22 21:36:01 +0100

    Update LoaderX.lua

--- LoaderX.lua
+++ LoaderX.lua
@@ -1,10 +1,14 @@
-local list = {
+local placeId = game.PlaceId
+
+local scripts = {
     ["7346416636"] = "https://raw.githubusercontent.com/SpxlZ/Spxlz_Hub12/main/Project1.lua",
     ["7903991471"] = "https://raw.githubusercontent.com/SpxlZ/Spxlz_Hub12/main/Project3.lua"
 }
 
-for id, url in next, list do
-    if string.find(id, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); break
-    end
+local scriptToLoad = scripts[tostring(placeId)]
+
+if scriptToLoad then
+    loadstring(game:HttpGet(scriptToLoad))()
+else
+    warn("Script not found for PlaceId:", placeId)
 end
